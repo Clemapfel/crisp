bd9ee92c281706c286598be9003bc9bd5ec9820f
clem <clemens.cords@aol.com>
2021-09-08 21:09:45 +0200

    added system header

---
+++ include/system.hpp
@@ -0,0 +1,12 @@
+// 
+// Copyright 2021 Clemens Cords
+// Created on 08.09.21 by clem (mail@clemens-cords.com)
+//
+
+#pragma once
+
+#include <system/image_io.hpp>
+#include <system/key_id.hpp>
+#include <system/input_handler.hpp>
+#include <system/render_window.hpp>
+#include <system/sprite.hpp>
