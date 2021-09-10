3b6fefda0d8d8cf6368383ee9340f92acd561a4b
clem <clemens.cords@aol.com>
2021-09-10 15:37:16 +0200

    added whole image transforms and pseudocolor

---
+++ include/pseudocolor_mapping.hpp
@@ -0,0 +1,11 @@
+// 
+// Copyright 2021 Clemens Cords
+// Created on 10.09.21 by clem (mail@clemens-cords.com)
+//
+
+#pragma once
+
+namespace crisp
+{
+
+}
---
+++ include/whole_image_transform.hpp
@@ -0,0 +1,27 @@
+// 
+// Copyright 2021 Clemens Cords
+// Created on 10.09.21 by clem (mail@clemens-cords.com)
+//
+
+#pragma once
+
+#include <image/multi_plane_image.hpp>
+
+namespace crisp
+{
+    // @brief only keep maxima in specified neighborhod
+    template<typename Image_t>
+    void non_maxima_surpress(Image_t&, size_t neighborhood_size);
+
+    // @brief apply histogram equalization
+    template<typename Image_t>
+    void histogram_equalize(Image_t&);
+
+    // @brief quantize into 8-bit, then split into bitplanes
+    std::vector<Image<bool, 1>> bitplane_decompose(const Image<float, 1>&);
+    Image<float, 1> bitplane_assemble(const std::vector<Image<bool, 1>>&);
+
+    // @brief wavelet transform
+    std::vector<Image<float, 1>> wavelet_decompose(const Image<float, 1>&);
+    Image<float, 1> wavelet_assemble(const std::vector<Image<bool, 1>>&);
+}
