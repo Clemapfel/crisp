8035f819a2edf5b452fea46bc6b15a1bee33baca
clem <clemens.cords@aol.com>
2021-09-14 15:59:56 +0200

    added documentation

--- include/pseudocolor_mapping.hpp
+++ include/pseudocolor_mapping.hpp
@@ -17,51 +17,83 @@ namespace crisp
     class PseudoColorTransform
     {
         public:
+            /// @brief default ctor
             PseudoColorTransform() = default;
 
             /// @brief transform a grayscale image into a color image my mapping certain gray values to the specified hues
+            /// @param image: grayscale image
+            /// @returns resulting color image
             [[nodiscard]] ColorImage transform(const GrayScaleImage&);
 
             /// @brief set the transform function
+            /// @param mapping: transform function
             void set_function(std::function<RGB(float)>&& mapping);
 
             /// @brief the identity function, maps gray value x to RBG(x, x, x, 1)
+            /// @returns function settable with set_function
             static auto&& identity();
 
             /// @brief map single gray value to single hue
-            //
-            /// @note equivalent to value_range_to_hue_range(gray, gray, hue, hue)
+            /// @param gray: origin gray value
+            /// @param hue: destination hue value
+            /// @returns function settable with set_function
             static auto&& value_to_hue(float gray, float hue);
 
             /// @brief map range of gray values onto single hue
-            //
-            /// @note equivalent to value_range_to_hue_range(min_gray, max_gray, hue, hue)
+            /// @param min_gray: lower inclusive boundary of gray values
+            /// @param max_gray: upper inclusive boundary of gray values
+            /// @param hue: destination hue value
+            /// @returns function settable with set_function
             static auto&& value_range_to_hue(float min_gray, float max_gray, float hue);
 
-            /// @brief maps grayvalues in [min_gray, max_gray] onto HSV values with hue in [min_hue, max_hue]
+            /// @brief map range of gray values [g_a, g_b] onto range of hues [h_a, h_b] such that g_a -> h_a, ..., g_b -> h_b
+            /// @param min_gray: lower inclusive boundary of gray values
+            /// @param max_gray: upper inclusive boundary of gray values
+            /// @param min_hue: lower inclusive boundary of hue values
+            /// @param max_hue: upper inclusive boundary of hue values
+            /// @returns function settable with set_function
             static auto&& value_range_to_hue_range(float min_gray, float max_gray, float min_hue = 0, float max_hue = 1);
 
-            /// @brief maps grayvalues in [min_gray, max_gray] onto HSV values with hue in [max_hue, min_hue]
+            /// @brief map range of gray values [g_a, g_b] onto range of hues [h_a, h_b] such that g_a -> h_b, ..., g_b -> h_a
+            /// @param min_gray: lower inclusive boundary of gray values
+            /// @param max_gray: upper inclusive boundary of gray values
+            /// @param min_hue: lower inclusive boundary of hue values
+            /// @param max_hue: upper inclusive boundary of hue values
+            /// @returns function settable with set_function
             static auto&& value_range_to_inverse_hue_range(float min_gray, float max_gray, float min_hue = 0, float max_hue = 1);
 
-            // container class that allows for specifying multiple ranges at the same time
+            // @brief container class that allows for specifying multiple ranges at the same time
             class RangeMapping
             {
                 friend class PseudoColorTransform;
 
                 public:
+                    /// @brief default ctor
                     RangeMapping() = default;
 
                     /// @brief add exact value to single hue mapping
+                    /// @param gray: origin gray value
+                    /// @param hue: destination hue value
                     void add_value_to_hue(float gray, float hue);
 
                     /// @brief add value range to single hue mapping
+                    /// @param min_gray: lower inclusive boundary of gray values
+                    /// @param max_gray: upper inclusive boundary of gray values
+                    /// @param hue: destination hue value
                     void add_value_range_to_hue(float min_gray, float max_gray, float hue);
 
                     /// @brief add value range to hue range mapping
+                    /// @param min_gray: lower inclusive boundary of gray values
+                    /// @param max_gray: upper inclusive boundary of gray values
+                    /// @param min_hue: lower inclusive boundary of hue values
+                    /// @param max_hue: upper inclusive boundary of hue values
                     void add_value_range_to_hue_range(float min_gray, float max_gray, float min_hue, float max_hue);
 
                     /// @brief add value range to inverse hue range mapping
+                    /// @param min_gray: lower inclusive boundary of gray values
+                    /// @param max_gray: upper inclusive boundary of gray values
+                    /// @param min_hue: lower inclusive boundary of hue values
+                    /// @param max_hue: upper inclusive boundary of hue values
                     void add_value_range_to_inverse_hue_range(float min_gray, float max_gray, float min_hue, float max_hue);
 
                 private:
@@ -70,6 +102,7 @@ namespace crisp
             };
 
             /// @brief map multiple gray intensity ranges onto multiple hue ranges simultaenously
+            /// @param mapping: mapping of type crisp::PseudoColor::RangeMapping
             static auto&& value_ranges_to_hue_ranges(RangeMapping& mapping);
 
         private:
--- include/segmentation.hpp
+++ include/segmentation.hpp
@@ -13,36 +13,75 @@
 namespace crisp
 {
     /// @brief extract all pixels of identical value results in number of segments equal to the number of pairwise different pixel values
+    /// @param image
+    /// @param min_segment_size: minimum size of segments, discard if size is equal or below minimum
+    /// @returns vector of resulting segments
     template<typename Image_t>
     std::vector<ImageSegment> decompose_into_segments(const Image_t&, size_t min_segment_size = 2);
 
-    /// @brief extract all 4-connected regions of identical value
+    /// @brief extract all 4-connected segments of identical value
+    /// @param image
+    /// @param min_segment_size: minimum size of segments, discard if size is equal or below minimum
+    /// @returns vector of resulting segments
     template<typename Image_t>
     std::vector<ImageSegment> decompose_into_connected_segments(const Image_t&, size_t min_segment_size = 2);
 
+    /// @brief compute threshold as specified
+    /// @param image
+    /// @param threshold the threshold, in [0, 1]
+    /// @returns thresholded image as binary
     template<typename Inner_t>
     BinaryImage manual_threshold(const Image<Inner_t, 1>&, Inner_t threshold);
 
+    /// @brief employ recursive heuristic to compute threshold
+    /// @param image
+    /// @returns thresholded image as binary
     template<typename Inner_t>
     BinaryImage basic_threshold(const Image<Inner_t>&);
 
+    /// @brief compute threshold that maximizes between-cluster variance, also called otsu's method
+    /// @param image
+    /// @returns thresholded image as binary
     template<typename Inner_t>
     BinaryImage otsu_threshold(const Image<Inner_t>&);
 
-    /// @param : image
-    /// @param tail_length_factor: range [0, 1+)
+    /// @brief compute local threshold by iterating through the image in a spiral pattern and remember only part of values visited so far
+    /// @param image
+    /// @param tail_lenght_factor: scales the number of remember elements, in [0, 1], (default: 0.05)
+    /// @returns thresholded image as binary
+    /// @complexity O(m * n + m * n * tail_length_factor)
     template<typename Inner_t>
     BinaryImage variable_threshold(const Image<Inner_t>&, float tail_length_factor = 0.05);
 
+    /// @brief compute local threshold by considering the pixels neighborhood
+    /// @param image
+    /// @param neighborhood_size: range of pixels considered (default: 5)
+    /// @returns thresholded image as binary
+    /// @complexity O(m*n*neighborhood_size)
     template<typename Inner_t>
     BinaryImage neighborhood_threshold(const Image<Inner_t>&, size_t neighborhood_size = 5);
 
+    /// @brief cluster image but create n superpixels and k-means clustering inside their boundaries
+    /// @param image: input image
+    /// @param n_superpixels
+    /// @param max_n_iterations: number of iterations allowed if convergence is not achieved earlier
+    /// @returns clustered image of same value type as input image
     template<typename Image_t>
     Image_t superpixel_clustering(const Image_t&, size_t n_superpixels, size_t max_n_iterations = std::numeric_limits<size_t>::max());
 
+    /// @brief cluster image by region growing
+    /// @param image: input image
+    /// @param seed_image: binary image with seeds
+    /// @returns clustered image of same value type as input image, unclustered values will be show up as black (grayscale) or magenta (color)
     template<typename Image_t>
     Image_t region_growing_clustering(const Image_t&, BinaryImage seed_image);
 
+    /// @brief cluster image using k-means with specified number of clusters
+    /// @param image: input image
+    /// @param n_clusters
+    /// @param max_n_iterations: number of iterations allowed if convergence is not achieved earlier
+    /// @returns clustered image of same value type as input image
+    /// @note for crisp::ColorImage specifically a high-quality heuristic is applied to maximize color-distance between clusters
     template<typename Image_t>
     Image_t k_means_clustering(const Image_t&, size_t n_clusters, size_t max_n_iterations = std::numeric_limits<size_t>::max());
 }
