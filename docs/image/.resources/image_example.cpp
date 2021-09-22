47d4fd143efd88e4ab0dd8b042235bb3ec4b2746
clem <clemens.cords@aol.com>
2021-09-22 18:33:11 +0200

    added noise docs

--- docs/image/.resources/image_example.cpp
+++ docs/image/.resources/image_example.cpp
@@ -5,13 +5,29 @@
 
 #include <system.hpp>
 #include <image/grayscale_image.hpp>
+#include <whole_image_transform.hpp>
 using namespace crisp;
 
 int main()
 {
 
-    const auto image = load_binary_image("/home/clem/Workspace/crisp/docs/image/color_opal.png");
-    save_to_disk(image, "/home/clem/Workspace/crisp/docs/imagebinary_opal.png");
+    auto image = load_grayscale_image("/home/clem/Workspace/crisp/docs/color/.resources/infrared_deer.png");
+
+    float min = 1.5, max = 0;
+    for (auto& px : image)
+    {
+        min = std::min<float>(px, min);
+        max = std::max<float>(px, max);
+    }
+
+    std::cout << min << " " << max << std::endl;
+
+    normalize(image);
+    save_to_disk(image, "/home/clem/Workspace/crisp/docs/image/.resources/deer_normalized.png");
+
+    auto gradient = compute_gradient_magnitude(image);
+    normalize(gradient);
+    save_to_disk(gradient, "/home/clem/Workspace/crisp/docs/image/.resources/deer_gradient.png");
 
     return 0;
 }
