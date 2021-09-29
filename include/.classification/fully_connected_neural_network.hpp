10767eb0a9114d19ce48d549722b2a1c1d0ab3a4
clem <clemens.cords@aol.com>
2021-09-29 21:18:18 +0200

    stashing

--- include/.classification/fully_connected_neural_network.hpp
+++ include/.classification/fully_connected_neural_network.hpp
@@ -44,18 +44,19 @@ namespace crisp
                 float activate(const std::vector<float>& previous_layer)
                 {
                     std::cout << "(" << _layer_i << ")" << std::endl;
-                    std::cout << "weights: " ;
                     for (auto& w : _weights)
-                        std::cout << w << " ";
-                    std::cout << _bias << "\n" << std::endl;
+                        std::cout << " " << w << " |";
+                    std::cout << "| " << _bias << std::endl;
 
                     float sum = 0;
                     for (size_t i = 0; i < previous_layer.size(); ++i)
                         sum += _weights.at(i) * previous_layer.at(i);
 
                     sum += _bias;
+                    float out = (1 / (1 + expf(-1 * sum)));
 
-                    return (1 / (1 + exp(-1 * sum)));
+                    std::cout << "-> " << out << "\n" << std::endl;
+                    return out;
                 }
 
                 //private:
@@ -87,6 +88,8 @@ namespace crisp
             }
 
             using FeatureVector_t = Vector<float, std::get<0>(std::make_tuple(LayerN...))>;
+            using OutputVector_t = Vector<float, std::get<sizeof...(LayerN)-1>(std::make_tuple(LayerN...))>;
+
 
             /// @returns pair where .first is the class, .second the response value
             std::pair<size_t, float> forward_pass(const FeatureVector_t& feature_vector)
@@ -122,15 +125,67 @@ namespace crisp
                     }
                 }
 
-                for (auto& vec : responses)
+                return std::make_pair(max_i, max_value);
+            }
+
+            void back_propagate(const FeatureVector_t& input, const OutputVector_t& desired_output)
+            {
+                // forward pass first
+                std::vector<std::vector<float>> observed;
+                observed.reserve(_layer_to_n.size());
+
+                observed.emplace_back();
+                observed.back().reserve(FeatureVector_t::size());
+
+                for (size_t i = 0; i < FeatureVector_t::size(); ++i)
+                    observed.back().push_back(input.at(i));
+
+                for (size_t layer_i = 1; layer_i < _layers.size(); ++layer_i)
                 {
-                    std::cout << std::endl;
-                    for (auto& value : vec)
-                        std::cout << value << " | ";
+                    observed.emplace_back();
+                    observed.back().reserve(_layer_to_n.at(layer_i));
+
+                    for (size_t neuron_i = 0; neuron_i < _layers.at(layer_i).size(); ++neuron_i)
+                        observed.back().push_back(
+                                _layers.at(layer_i).at(neuron_i).activate(observed.at(layer_i - 1)));
+                }
+
+                // init error
+                std::vector<std::vector<float>> error;
+                for (size_t i = 0; i < _layers.size(); ++i)
+                {
+                    error.emplace_back();
+                    for (size_t j = 0; j < _layers.at(i).size(); ++j)
+                        error.back().push_back(-1);
+                }
+
+                // last layer
+                for (size_t i = 0; i < error.back().size(); ++i)
+                {
+                    float hzj = observed.at(observed.size() - 2).at(i);
+                    error.at(i) = hzj * (1 - hzj) * observed.back().at(i) - desired_output.at(i);
+                }
+
+                // back propagate
+                for (size_t layer_i = _layers.size() - 2; layer_i >= 0; layer_i--)
+                {
+                    for (size_t node_i = 0; node_i < error.at(layer_i).size(); node_i++)
+                    {
+                        float hzj = observed.at(layer_i).at(node_i);
+
+                        float delta = 0;
+                        for (size_t weight_i = 0; weight_i < _layers.at(layer_i-1).size(); ++weight_i)
+                        {
+                            delta +=
+                        }
+                    }
                 }
 
 
-                return std::make_pair(max_i, max_value);
+
+
+
+
             }
     };
 }
