96ca59bcbc369eb56dbef27bde2dc41207733c79
clem <clemens.cords@aol.com>
2021-09-29 20:17:10 +0200

    first draft forward pass neural network

--- .test/main.cpp
+++ .test/main.cpp
@@ -3,52 +3,15 @@
 // Created on 07.09.21 by clem (mail@clemens-cords.com)
 //
 
-#include <vector.hpp>
-#include <image.hpp>
-#include <color.hpp>
-#include <image.hpp>
-#include <system/image_io.hpp>
-#include <system/sprite.hpp>
-#include <system/render_window.hpp>
-#include <spatial_filter.hpp>
-#include <morphological_transform.hpp>
-#include <pseudocolor_mapping.hpp>
-#include <fourier_transform.hpp>
-#include <edge_detection.hpp>
-#include <segmentation.hpp>
-#include <whole_image_transform.hpp>
-
-#include <noise_generator.hpp>
-
-
-#include <iostream>
+#include <.classification/fully_connected_neural_network.hpp>
 
 using namespace crisp;
 
 int main()
 {
-    auto image = load_grayscale_image("/home/clem/Workspace/crisp/docs/frequency_domain/color_opal.png");
+    auto nn = NeuralNetwork<1, 2, 3>();
 
-    sf::Clock clock;
-    auto fourier = FourierTransform<ACCURACY>();
-    fourier.transform_from(image);
-    auto res = fourier.transform_to<GrayScaleImage>();
-    std::cout << clock.restart().asSeconds() << std::endl;
+    std::cout << nn.forward_pass({2}).second << std::endl;
 
-    auto sprite = Sprite();
-    sprite.create_from(res);
-    sprite.set_scale(2);
-
-    auto size = sprite.get_size();
-    auto window = RenderWindow(sprite.get_size().x(), sprite.get_size().y());
-
-    while (window.is_open())
-    {
-        auto time = window.update();
-
-        window.clear();
-        window.draw(sprite);
-        window.display();
-    }
 }
 
--- include/.classification/fully_connected_neural_network.hpp
+++ include/.classification/fully_connected_neural_network.hpp
@@ -4,3 +4,114 @@
 //
 
 #pragma once
+
+#include <vector.hpp>
+#include <iostream>
+
+namespace crisp
+{
+    // first layer: number of elements in feature vector
+    // hidden layers: arbitrary
+    // last layer: number of observed classes
+    template<size_t... LayerN>
+    struct NeuralNetwork
+    {
+        static_assert(sizeof...(LayerN) >= 3, "Specify at least 3 layers");
+
+        private:
+            const static inline std::vector<size_t> _layer_to_n = {LayerN...};
+
+            struct Neuron
+            {
+                Neuron(size_t layer_i, size_t previous_layer_n)
+                    : _layer_i(layer_i)
+                {
+                    _weights.clear();
+
+                    if (layer_i == 0)
+                    {
+                        _weights.resize(1, 1);
+                        _bias = 0;
+                    }
+                    else
+                    {
+                        _weights.resize(previous_layer_n, 1);
+                        _bias = 1;
+                    }
+                }
+
+                // updates response
+                float activate(const std::vector<float>& previous_layer)
+                {
+                    float sum = 0;
+                    for (size_t i = 0; i < previous_layer.size(); ++i)
+                        sum += _weights.at(i) * previous_layer.at(i);
+
+                    sum += _bias;
+
+                    return (1 / (expf(-1 * sum)));
+                }
+
+                //private:
+                    std::vector<float> _weights;
+                    float _bias;
+                    size_t _layer_i;
+            };
+
+            std::vector<std::vector<Neuron>> _layers;
+
+        public:
+            NeuralNetwork()
+            {
+                for (size_t layer_i = 0; layer_i < _layer_to_n.size(); ++layer_i)
+                {
+                    _layers.emplace_back();
+                    _layers.back().resize(_layer_to_n.at(layer_i), Neuron(layer_i, (layer_i == 0 ? 0 : _layer_to_n.at(layer_i - 1))));
+                }
+            }
+
+            void override_weight(size_t layer_i, size_t neuron_i, size_t weight_i, float new_weight)
+            {
+                _layers.at(layer_i).at(neuron_i)._weights.at(weight_i) = new_weight;
+            }
+
+            using FeatureVector_t = Vector<float, std::get<0>(std::make_tuple(LayerN...))>;
+
+            /// @returns pair where .first is the class, .second the response value
+            std::pair<size_t, float> forward_pass(const FeatureVector_t& feature_vector)
+            {
+                std::vector<std::vector<float>> responses;
+                responses.reserve(_layer_to_n.size());
+
+                responses.emplace_back();
+                responses.back().reserve(FeatureVector_t::size());
+
+                for (size_t i = 0; i < FeatureVector_t::size(); ++i)
+                    responses.back().push_back(feature_vector);
+
+                for (size_t layer_i = 1; layer_i < _layers.size(); ++layer_i)
+                {
+                    responses.emplace_back();
+                    responses.back().reserve(_layer_to_n.at(layer_i));
+
+                    for (size_t neuron_i = 0; neuron_i < _layers.at(layer_i).size(); ++neuron_i)
+                        responses.back().push_back(
+                                _layers.at(layer_i).at(neuron_i).activate(responses.at(layer_i - 1)));
+                }
+
+                size_t max_i = -1;
+                float max_value = std::numeric_limits<float>::min();
+                for (size_t i = 0; i < _layer_to_n.back(); ++i)
+                {
+                    float value = responses.back().at(i);
+                    if (value > max_value)
+                    {
+                        max_i = i;
+                        max_value = value;
+                    }
+                }
+
+                return std::make_pair(max_i, max_value);
+            }
+    };
+}
