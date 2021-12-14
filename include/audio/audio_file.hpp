45f305de07c64fdeee7115656c6791f583c7eedc
clem <clemens.cords@aol.com>
2021-12-14 20:43:53 +0100

    added documentation

--- include/audio/audio_file.hpp
+++ include/audio/audio_file.hpp
@@ -17,24 +17,43 @@ namespace crisp
     class AudioFile
     {
         public:
+            /// @brief default ctor
             AudioFile();
 
+            /// @brief load from file
+            /// @param path: absolute path
+            /// @note: supported formats: .wav, .ogg
             void load(std::string path);
+
+            /// @brief save to file
+            /// @param path: absolute path
+            /// @note: supported formats: .wav, .ogg
             void save(std::string path);
 
+            /// @brief get number of samples in entire audio file
+            /// @returns size_t
             size_t get_n_samples() const;
+
+            /// @brief get number of channels
+            /// @returns 1 for mono, 2 for stereo, >2 for surround
             size_t get_n_channels() const;
 
-            /// @returns duration in seconds
+            /// @param get duration in seconds
+            /// @returns number of seconds
             float get_duration() const;
 
+            /// @param get sample rate
             /// @returns number of samples per second
             size_t get_sample_rate() const;
 
-            sf::Sound as_sound() const;
-
-            /// @returns vector of float with values in range [-1, 1]
+            /// @param normalize signal into range [-1, 1] and return as vector
+            /// @returns vector of floats
             [[nodiscard]] std::vector<float> get_samples() const;
+
+        protected:
+            /// @brief transform to playable sound
+            /// @returns sf::Sound
+            sf::Sound as_sound() const;
 
         private:
             sf::SoundBuffer _buffer;
